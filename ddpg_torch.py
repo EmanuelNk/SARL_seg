330e057c0860cf583f5c6eed5832394b188447b3
EmanuelNk <emanuelnn30@gmail.com>
2022-09-08 13:44:17 +0300

    added python script for ddpg

--- ddpg_torch.py
+++ ddpg_torch.py
@@ -1,9 +1,20 @@
 import os
-import torch as T
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torchvision.transforms import Compose, Resize, ToTensor
 import numpy as np
+import torchvision.transforms as T
+from PIL import Image, ImageOps
+
+# resize = Compose([torch.ToPILImage(),
+#                     torch.Resize(112, interpolation=Image.CUBIC),
+#                     torch.ToTensor()])
+
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(112, interpolation=Image.CUBIC),
+                    T.ToTensor()])
 
 class OUActionNoise(object):
     def __init__(self, mu, sigma=0.15, theta=.2, dt=1e-2, x0=None):
@@ -68,10 +79,19 @@ class CriticNetwork(nn.Module):
         self.fc2_dims = fc2_dims
         self.n_actions = n_actions
         self.checkpoint_file = os.path.join(chkpt_dir,name+'_ddpg')
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.con_bn1 = nn.GroupNorm(16,16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.con_bn2 = nn.GroupNorm(32,32)
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(112))
+        convh = conv2d_size_out(conv2d_size_out(112))
+        linear_input_size = convw * convh * 32
+        self.fc1 = nn.Linear(linear_input_size, self.fc1_dims)
         f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
-        T.nn.init.uniform_(self.fc1.weight.data, -f1, f1)
-        T.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
+        torch.nn.init.uniform_(self.fc1.weight.data, -f1, f1)
+        torch.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
         #self.fc1.weight.data.uniform_(-f1, f1)
         #self.fc1.bias.data.uniform_(-f1, f1)
         self.bn1 = nn.LayerNorm(self.fc1_dims)
@@ -79,8 +99,8 @@ class CriticNetwork(nn.Module):
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
         f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
         #f2 = 0.002
-        T.nn.init.uniform_(self.fc2.weight.data, -f2, f2)
-        T.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
+        torch.nn.init.uniform_(self.fc2.weight.data, -f2, f2)
+        torch.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
         #self.fc2.weight.data.uniform_(-f2, f2)
         #self.fc2.bias.data.uniform_(-f2, f2)
         self.bn2 = nn.LayerNorm(self.fc2_dims)
@@ -88,36 +108,40 @@ class CriticNetwork(nn.Module):
         self.action_value = nn.Linear(self.n_actions, self.fc2_dims)
         f3 = 0.003
         self.q = nn.Linear(self.fc2_dims, 1)
-        T.nn.init.uniform_(self.q.weight.data, -f3, f3)
-        T.nn.init.uniform_(self.q.bias.data, -f3, f3)
+        torch.nn.init.uniform_(self.q.weight.data, -f3, f3)
+        torch.nn.init.uniform_(self.q.bias.data, -f3, f3)
         #self.q.weight.data.uniform_(-f3, f3)
         #self.q.bias.data.uniform_(-f3, f3)
 
         self.optimizer = optim.Adam(self.parameters(), lr=beta)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cuda:1')
 
         self.to(self.device)
 
     def forward(self, state, action):
-        state_value = self.fc1(state)
+        state_value = F.relu(self.con_bn1(self.conv1(state)))
+        state_value = self.conv2(state_value)
+        state_value = self.con_bn2(state_value)
+        state_value = F.relu(state_value)
+        state_value = self.fc1(state_value.view(state_value.size(0), -1))
         state_value = self.bn1(state_value)
         state_value = F.relu(state_value)
         state_value = self.fc2(state_value)
         state_value = self.bn2(state_value)
 
         action_value = F.relu(self.action_value(action))
-        state_action_value = F.relu(T.add(state_value, action_value))
+        state_action_value = F.relu(torch.add(state_value, action_value))
         state_action_value = self.q(state_action_value)
 
         return state_action_value
 
     def save_checkpoint(self):
         print('... saving checkpoint ...')
-        T.save(self.state_dict(), self.checkpoint_file)
+        torch.save(self.state_dict(), self.checkpoint_file)
 
     def load_checkpoint(self):
         print('... loading checkpoint ...')
-        self.load_state_dict(T.load(self.checkpoint_file))
+        self.load_state_dict(torch.load(self.checkpoint_file))
 
 class ActorNetwork(nn.Module):
     def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions, name,
@@ -127,11 +151,20 @@ class ActorNetwork(nn.Module):
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
         self.n_actions = n_actions
-        self.checkpoint_file = os.path.join(chkpt_dir,name+'_ddpg')
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.checkpoint_file = '/'.join([chkpt_dir,name+'_ddpg'])
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.con_bn1 = nn.GroupNorm(16,16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.con_bn2 = nn.GroupNorm(32,32)
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(112))
+        convh = conv2d_size_out(conv2d_size_out(112))
+        linear_input_size = convw * convh * 32
+        self.fc1 = nn.Linear(linear_input_size, self.fc1_dims)
         f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
-        T.nn.init.uniform_(self.fc1.weight.data, -f1, f1)
-        T.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
+        torch.nn.init.uniform_(self.fc1.weight.data, -f1, f1)
+        torch.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
         #self.fc1.weight.data.uniform_(-f1, f1)
         #self.fc1.bias.data.uniform_(-f1, f1)
         self.bn1 = nn.LayerNorm(self.fc1_dims)
@@ -139,8 +172,8 @@ class ActorNetwork(nn.Module):
         self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
         #f2 = 0.002
         f2 = 1./np.sqrt(self.fc2.weight.data.size()[0])
-        T.nn.init.uniform_(self.fc2.weight.data, -f2, f2)
-        T.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
+        torch.nn.init.uniform_(self.fc2.weight.data, -f2, f2)
+        torch.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
         #self.fc2.weight.data.uniform_(-f2, f2)
         #self.fc2.bias.data.uniform_(-f2, f2)
         self.bn2 = nn.LayerNorm(self.fc2_dims)
@@ -148,38 +181,49 @@ class ActorNetwork(nn.Module):
         #f3 = 0.004
         f3 = 0.003
         self.mu = nn.Linear(self.fc2_dims, self.n_actions)
-        T.nn.init.uniform_(self.mu.weight.data, -f3, f3)
-        T.nn.init.uniform_(self.mu.bias.data, -f3, f3)
+        torch.nn.init.uniform_(self.mu.weight.data, -f3, f3)
+        torch.nn.init.uniform_(self.mu.bias.data, -f3, f3)
         #self.mu.weight.data.uniform_(-f3, f3)
         #self.mu.bias.data.uniform_(-f3, f3)
 
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cuda:1')
 
         self.to(self.device)
 
     def forward(self, state):
-        x = self.fc1(state)
+        x = self.conv1(state)
+        x = self.con_bn1(x)
+        x = F.relu(x)
+        x = F.relu(self.con_bn2(self.conv2(x)))
+        x = self.fc1(x.view(x.size(0), -1))
         x = self.bn1(x)
         x = F.relu(x)
         x = self.fc2(x)
         x = self.bn2(x)
         x = F.relu(x)
-        x = T.tanh(self.mu(x))
+        x = torch.tanh(self.mu(x))
 
         return x
 
     def save_checkpoint(self):
         print('... saving checkpoint ...')
-        T.save(self.state_dict(), self.checkpoint_file)
+        torch.save(self.state_dict(), self.checkpoint_file)
 
     def load_checkpoint(self):
         print('... loading checkpoint ...')
-        self.load_state_dict(T.load(self.checkpoint_file))
+        self.load_state_dict(torch.load(self.checkpoint_file))
+
+def transform_obs(observation):
+        observation = np.ascontiguousarray(observation, dtype=np.float32) / 255
+        observation = torch.from_numpy(observation)
+        observation = resize(observation.T)
+        observation = observation.unsqueeze(0)
+        return observation
 
 class Agent(object):
-    def __init__(self, alpha, beta, input_dims, tau, env, gamma=0.99,
-                 n_actions=2, max_size=1000000, layer1_size=400,
+    def __init__(self, alpha, beta, input_dims, tau, gamma=0.99,
+                 n_actions=2, max_size=10000, layer1_size=400,
                  layer2_size=300, batch_size=64):
         self.gamma = gamma
         self.tau = tau
@@ -204,12 +248,17 @@ class Agent(object):
 
         self.update_network_parameters(tau=1)
 
+    
+    
+
     def choose_action(self, observation):
         self.actor.eval()
-        observation = T.tensor(observation, dtype=T.float).to(self.actor.device)
-        mu = self.actor.forward(observation).to(self.actor.device)
-        mu_prime = mu + T.tensor(self.noise(),
-                                 dtype=T.float).to(self.actor.device)
+        observation = transform_obs(observation)
+        
+        # observation = torch.tensor(observation, dtype=torch.float).to(self.actor.device)
+        mu = self.actor.forward(observation.to(self.actor.device)).to(self.actor.device)
+        mu_prime = mu + torch.tensor(self.noise(),
+                                 dtype=torch.float).to(self.actor.device)
         self.actor.train()
         return mu_prime.cpu().detach().numpy()
 
@@ -223,11 +272,11 @@ class Agent(object):
         state, action, reward, new_state, done = \
                                       self.memory.sample_buffer(self.batch_size)
 
-        reward = T.tensor(reward, dtype=T.float).to(self.critic.device)
-        done = T.tensor(done).to(self.critic.device)
-        new_state = T.tensor(new_state, dtype=T.float).to(self.critic.device)
-        action = T.tensor(action, dtype=T.float).to(self.critic.device)
-        state = T.tensor(state, dtype=T.float).to(self.critic.device)
+        reward = torch.tensor(reward, dtype=torch.float).to(self.critic.device)
+        done = torch.tensor(done).to(self.critic.device)
+        new_state = torch.tensor(new_state, dtype=torch.float).to(self.critic.device)
+        action = torch.tensor(action, dtype=torch.float).to(self.critic.device)
+        state = torch.tensor(state, dtype=torch.float).to(self.critic.device)
 
         self.target_actor.eval()
         self.target_critic.eval()
@@ -239,7 +288,7 @@ class Agent(object):
         target = []
         for j in range(self.batch_size):
             target.append(reward[j] + self.gamma*critic_value_[j]*done[j])
-        target = T.tensor(target).to(self.critic.device)
+        target = torch.tensor(target).to(self.critic.device)
         target = target.view(self.batch_size, 1)
 
         self.critic.train()
@@ -253,7 +302,7 @@ class Agent(object):
         mu = self.actor.forward(state)
         self.actor.train()
         actor_loss = -self.critic.forward(state, mu)
-        actor_loss = T.mean(actor_loss)
+        actor_loss = torch.mean(actor_loss)
         actor_loss.backward()
         self.actor.optimizer.step()
 
@@ -293,10 +342,10 @@ class Agent(object):
         actor_state_dict = dict(target_actor_params)
         print('\nActor Networks', tau)
         for name, param in self.actor.named_parameters():
-            print(name, T.equal(param, actor_state_dict[name]))
+            print(name, torch.equal(param, actor_state_dict[name]))
         print('\nCritic Networks', tau)
         for name, param in self.critic.named_parameters():
-            print(name, T.equal(param, critic_state_dict[name]))
+            print(name, torch.equal(param, critic_state_dict[name]))
         input()
         """
     def save_models(self):
@@ -321,8 +370,8 @@ class Agent(object):
         print('Checking Actor parameters')
 
         for param in current_actor_dict:
-            print(param, T.equal(original_actor_dict[param], current_actor_dict[param]))
+            print(param, torch.equal(original_actor_dict[param], current_actor_dict[param]))
         print('Checking critic parameters')
         for param in current_critic_dict:
-            print(param, T.equal(original_critic_dict[param], current_critic_dict[param]))
+            print(param, torch.equal(original_critic_dict[param], current_critic_dict[param]))
         input()
